d7c81e9398d1f81d6ee6a29aa242a77254089f77
karlarn <karlarnichols@gmail.com>
2022-05-23 14:57:12 -0500

    Phase8

--- Program.cs
+++ Program.cs
@@ -8,19 +8,43 @@ namespace Guessing_game
         static void Main(string[] args)
         {
             Console.WriteLine("Choose your difficulty: Easy, Medium, Hard.");
-            string difficulty=Console.ReadLine().ToLower();
-            int numberToGuess=0;
-            if (difficulty=="easy"){
-                 numberToGuess=8;
+            string difficulty = Console.ReadLine().ToLower();
+            int numberToGuess = 0;
+            if (difficulty == "easy")
+            {
+                numberToGuess = 8;
             }
-            else if(difficulty=="medium"){
-                numberToGuess=6;
+            else if (difficulty == "medium")
+            {
+                numberToGuess = 6;
             }
-            else if(difficulty=="hard"){
-                numberToGuess=4;
+            else if (difficulty == "hard")
+            {
+                numberToGuess = 4;
+            }
+            else if (difficulty == "cheater")
+            {
+                Console.WriteLine("Guess the secret number!");
+                int sNumber = new Random().Next(1, 101);
+                int guessedNumber = Int32.Parse(Console.ReadLine());
+                while (sNumber != guessedNumber)
+                {
+                    if (sNumber > guessedNumber)
+                    {
+                        Console.WriteLine("Guess higher!");
+                        guessedNumber = Int32.Parse(Console.ReadLine());
+                    }
+                    else if (sNumber < guessedNumber)
+                    {
+                        Console.WriteLine("Guess lower!");
+                        guessedNumber = Int32.Parse(Console.ReadLine());
+                    }
+                }
+                Console.WriteLine("You guessed it!");
+                return;
             }
             Console.WriteLine("Guess the secret number!");
-            int secretNumber = new Random().Next(1,101);
+            int secretNumber = new Random().Next(1, 101);
             for (int i = 1; i <= numberToGuess; i++)
             {
                 Console.WriteLine($"You have {numberToGuess} guesses. You're on {i}.");
@@ -32,10 +56,12 @@ namespace Guessing_game
                 }
                 else
                 {
-                    if(secretNumber>guess){
+                    if (secretNumber > guess)
+                    {
                         Console.WriteLine("Guess higher!");
                     }
-                    else{
+                    else
+                    {
                         Console.WriteLine("Guess lower!");
                     }
 
